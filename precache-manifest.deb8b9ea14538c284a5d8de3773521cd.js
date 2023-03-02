d174d6a35de56fa5e78f825fb425bd65ac0c8ed3
Adarsh Mohanty <am2475@srmist.edu.in>
2023-03-03 01:51:53 +0530

    Updates

---
+++ precache-manifest.deb8b9ea14538c284a5d8de3773521cd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc88e70a642d2f66a004",
+    "url": "/rydeu/static/css/main.84ef41c5.chunk.css"
+  },
+  {
+    "revision": "fc88e70a642d2f66a004",
+    "url": "/rydeu/static/js/main.fc88e70a.chunk.js"
+  },
+  {
+    "revision": "23e5026d3c124a934bc7",
+    "url": "/rydeu/static/js/1.23e5026d.chunk.js"
+  },
+  {
+    "revision": "04741fe15ae7c5ae448e",
+    "url": "/rydeu/static/js/2.04741fe1.chunk.js"
+  },
+  {
+    "revision": "96da6a3669f97d7b9a37",
+    "url": "/rydeu/static/js/runtime~main.96da6a36.js"
+  },
+  {
+    "revision": "884633de55e7194f098c64bf1bfcb7bc",
+    "url": "/rydeu/static/media/cover1.884633de.jpg"
+  },
+  {
+    "revision": "5efd9982a2b5950f7588674497d6736e",
+    "url": "/rydeu/static/media/bus.5efd9982.png"
+  },
+  {
+    "revision": "77737c2d4bcd8529b51fc04a410f1b78",
+    "url": "/rydeu/static/media/bookPay.77737c2d.png"
+  },
+  {
+    "revision": "c18029861423aa5d9bee7a0988a530d6",
+    "url": "/rydeu/static/media/mainContent.c1802986.png"
+  },
+  {
+    "revision": "b350a5d1f122af85fe7816f9190c1f60",
+    "url": "/rydeu/static/media/footerays.b350a5d1.png"
+  },
+  {
+    "revision": "567403937628dfcc72d184cedb3e6f78",
+    "url": "/rydeu/index.html"
+  }
+];
